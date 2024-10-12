78cb4256dabc7daf6749eae3ae9dd33f6f99eece
Ryan Silva <nrganeninja@gmail.com>
2024-10-12 02:43:27 +0100

    First Commit

---
+++ src/components/Experience.jsx
@@ -0,0 +1,22 @@
+import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import { Avatar } from "./Avatar";
+import { useThree } from "@react-three/fiber";
+
+export const Experience = () => {
+
+  const texture = useTexture("textures/room12.jpg");
+  const viewport = useThree((state) => state.viewport);
+
+
+  return (
+    <>
+      <OrbitControls />
+      <Avatar position={[0, -3, 5]} scale={2} />
+      <Environment preset="sunset" />
+      <mesh>
+        <planeGeometry args={[viewport.width, viewport.height]} />
+        <meshBasicMaterial map={texture} />
+      </mesh>
+    </>
+  );
+};
